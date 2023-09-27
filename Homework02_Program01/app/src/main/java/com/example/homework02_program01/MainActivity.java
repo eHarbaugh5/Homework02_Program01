e4c557add8e1a388b5dd60d7c98d842296636b4d
eHarbaugh5 <129325140+eHarbaugh5@users.noreply.github.com>
2023-09-27 10:35:48 -0400

    Initial Commit
    Creation of Homework02_Program01

---
+++ Homework02_Program01/app/src/main/java/com/example/homework02_program01/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.homework02_program01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
